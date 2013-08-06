49c806d4b2728d0d335621ee474da81ecf3d2e66
cedricms <cedric@maria-sube.com>
2013-08-06 23:09:18 +0200

    develop : clean up of the gen directory.

--- gen/com/maria_sube/cedric/mobileframezerotools/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.maria_sube.cedric.mobileframezerotools;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
