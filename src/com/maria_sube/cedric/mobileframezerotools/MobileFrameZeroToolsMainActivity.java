ee1a0c5dd7109a3c1883a00730a2bc5631b741ce
cedricms <cedric@maria-sube.com>
2013-08-06 21:33:13 +0200

    phone_gap_project : PhoneGap is activated

--- src/com/maria_sube/cedric/mobileframezerotools/MobileFrameZeroToolsMainActivity.java
+++ src/com/maria_sube/cedric/mobileframezerotools/MobileFrameZeroToolsMainActivity.java
@@ -1,22 +1,25 @@
 package com.maria_sube.cedric.mobileframezerotools;
 
+import org.apache.cordova.DroidGap;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 
-public class MobileFrameZeroToolsMainActivity extends Activity {
+public class MobileFrameZeroToolsMainActivity extends DroidGap {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_mobile_frame_zero_tools_main);
+        //setContentView(R.layout.activity_mobile_frame_zero_tools_main);
+        super.loadUrl("file:///android_asset/www/index.html");
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.activity_mobile_frame_zero_tools_main, menu);
-        return true;
-    }
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.activity_mobile_frame_zero_tools_main, menu);
+//        return true;
+//    }
     
 }
