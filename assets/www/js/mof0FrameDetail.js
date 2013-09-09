3b900e101028a692276cdf7e775b37eb3b32df01
cedricms <cedric@maria-sube.com>
2013-09-10 00:25:02 +0200

    frame_detail : added aut-update of the systems' count.

--- assets/www/js/mof0FrameDetail.js
+++ assets/www/js/mof0FrameDetail.js
@@ -1,7 +1,66 @@
+var systemsMessage = "";
+
 function getNbSystems() {
-	return 0;
+	result = 0;
+	
+	frameForm = document.getElementById("frameForm");
+
+	defensiveSystem = frameForm.elements["defensiveSystem"];	
+	result = result + getRadioIntValue(defensiveSystem);
+
+	movementSystem = frameForm.elements["movementSystem"];
+	result = result + getRadioIntValue(movementSystem);
+
+	surveillanceCommunicationSystem = frameForm.elements["surveillanceCommunicationSystem"];
+	result = result + getRadioIntValue(surveillanceCommunicationSystem);
+
+	handToHandWeaponSystem = frameForm.elements["handToHandWeaponSystem"];
+	result = result + getRadioIntValue(handToHandWeaponSystem);
+
+	directFireWeaponSystem = frameForm.elements["directFireWeaponSystem"];
+	result = result + getRadioIntValue(directFireWeaponSystem);
+
+	artilleryRangeWeaponSystem = frameForm.elements["artilleryRangeWeaponSystem"];
+	result = result + getRadioIntValue(artilleryRangeWeaponSystem);
+	
+	return result;
 }
 
-function getSystemsLabel(systemsMessage) {
+function getRadioIntValue(nodeList) {
+  result = 0;
+
+  for (i=0; i < nodeList.length; i++) {
+    if (nodeList[i].checked) {
+      nodeValue = nodeList[i].value;
+      if (!isNaN(nodeValue)) {
+        result = parseInt(nodeValue);
+        break;
+      } // if
+    } // if
+  } // for
+  
+  //alert(nodeList.name + " : " + result);
+  
+  return result;
+}
+
+function getSystemsLabel(pSystemsMessage) {
+	systemsMessage = pSystemsMessage;
 	return systemsMessage + " (" + getNbSystems() + " / 4)";
+}
+
+function updateFrame() {
+	// Update dice
+	
+	// Update system count
+	//$("#systemsLabel").text(htmlToSpecialCharacters(getSystemsLabel(systemsMessage))).html();
+	$("#systemsLabel").text(getSystemsLabel(systemsMessage));
+}
+
+function htmlToSpecialCharacters(htmlString) {
+	result = htmlString;
+	
+	result = result.replace("&egrave;", "è");
+	
+	return result;
 }
