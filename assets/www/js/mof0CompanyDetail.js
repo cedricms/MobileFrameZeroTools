90b1b1c3ccecf097c5eb5e95756e7ee09a894344
cedricms <cedric@maria-sube.com>
2013-10-02 00:32:42 +0200

    company_detail : reload the company information.

--- assets/www/js/mof0CompanyDetail.js
+++ assets/www/js/mof0CompanyDetail.js
@@ -69,6 +69,62 @@ function errorDB(tx, err) {
 	alert("Error processing SQL: "+err);
 }
 
+function loadCompany(urlCompanyId) {
+	companyForm = document.getElementById("companyForm");
+	
+	companyId = companyForm.elements["companyId"];
+	
+	if (urlCompanyId === undefined) {
+		// Do nothing
+	}
+	else {
+		companyId.value = urlCompanyId;
+		var db = window.openDatabase("mof0DB", dbVersion, "Mobile Frame Zero Tools", 200000);
+		db.transaction(queryCompanyId, errorDB);
+	} // if
+}
+
+// Query the database
+function queryCompanyId(tx) {
+	companyForm = document.getElementById("companyForm");
+    companyId = companyForm.elements["companyId"];
+    
+    tx.executeSql('SELECT * FROM company WHERE id=?', [companyId.value], queryCompanyIdSuccess, errorDB);
+}
+
+// Query the success callback
+function queryCompanyIdSuccess(tx, results) {
+    var len = results.rows.length;
+    
+    companyForm = document.getElementById("companyForm");
+    
+    for (var i=0; i<len; i++){
+    	var row = results.rows.item(i);
+    	
+    	companyName = companyForm.elements["companyName"];
+    	companyName.value = row.name;
+    	
+    	tx.executeSql('SELECT f.id AS frameId, f.name AS frameName, nb_defensive, nb_movement, nb_surveillance_communication, nb_hand_to_hand, nb_direct_fire, nb_artillery_range, nb_rockets FROM company_frame cf, frame f WHERE cf.id_company=? AND cf.id_frame=f.id ORDER BY f.name', [row.id], queryFrameForRowSuccess, errorDB);
+    	
+    	break;
+    } // for
+}
+
+function queryFrameForRowSuccess(tx, results) {
+	var len = results.rows.length;
+    
+    for (var i=0; i<len; i++){
+    	var row = results.rows.item(i);
+		addFrameRow(row);
+    
+    	companyForm = document.getElementById("companyForm");
+    	
+    	nbFrame = i + 1;
+    	nbRockets = companyForm.elements["nbRockets_" + nbFrame];
+    	nbRockets.value = row.nb_rockets;
+	} // for
+}
+
 function createCompany() {
 	var db = window.openDatabase("mof0DB", dbVersion, "Mobile Frame Zero Tools", 200000);
 	db.transaction(queryCreateCompany, errorDB);
@@ -89,6 +145,8 @@ function addFramesToCompany(companyId) {
 	nbFrames = parseInt(companyForm.elements["nbFrames"].value);
 	
 	if (nbFrames > 0) {
+		sqlInsertCompanyFrame = 'INSERT INTO company_frame (id_company, id_frame, nb_rockets) ';
+		
 		iFrame = 1;
 		while (iFrame <= nbFrames) {
 			companyForm = document.getElementById("companyForm");
@@ -100,22 +158,31 @@ function addFramesToCompany(companyId) {
 				if (!isNaN(frameId.value)) {
         			frameIdInt = parseInt(frameId.value);
         			
-        			nbRockets = companyForm.elements["nbRockets_" + iFrame].value;
+        			nbRockets = companyForm.elements["nbRockets_" + iFrame];
         			nbRocketsInt = 0;
         			if (!isNaN(nbRockets.value)) {
         				nbRocketsInt = parseInt(nbRockets.value);
         			} // if
-        			
-        			sqlInsertCompanyFrame = 'INSERT INTO company_frame (id_company, id_frame, nb_rockets) VALUES (?, ?, ?)';
-					var db = window.openDatabase("mof0DB", dbVersion, "Mobile Frame Zero Tools", 200000);
-					db.transaction(function(tx) {
-						tx.executeSql(sqlInsertCompanyFrame,[companyId, frameIdInt, nbRocketsInt]);
-					}, errorDB, successDB);
         		} // if
 			} // if
 			
+			if (iFrame > 1) {
+				sqlInsertCompanyFrame = sqlInsertCompanyFrame + ' UNION ALL ';
+			} // if
+			
+			sqlInsertCompanyFrame = sqlInsertCompanyFrame + 'SELECT ' + companyId + ', ' + frameIdInt + ', ' + nbRocketsInt;
+					
 			iFrame++;
 		} // while
+		
+		sqlInsertCompanyFrame = sqlInsertCompanyFrame + ';';
+		
+		//alert('sqlInsertCompanyFrame : ' + sqlInsertCompanyFrame);
+		
+		var db = window.openDatabase("mof0DB", dbVersion, "Mobile Frame Zero Tools", 200000);
+		db.transaction(function(tx) {
+			tx.executeSql(sqlInsertCompanyFrame,[]);
+		}, errorDB, successDB);
 	} // if
 }
 
@@ -204,17 +271,23 @@ function queryAddFrameToCompany(tx) {
 	
 	frameToAddId = getSelectedOptionIntValue(frameToAdd);
 	
-    tx.executeSql('SELECT * FROM frame WHERE id=?', [frameToAddId], queryAddFrameToCompanySuccess, errorDB);
+    tx.executeSql('SELECT f.id AS frameId, f.name AS frameName, nb_defensive, nb_movement, nb_surveillance_communication, nb_hand_to_hand, nb_direct_fire, nb_artillery_range FROM frame f WHERE f.id=?', [frameToAddId], queryAddFrameToCompanySuccess, errorDB);
 }
 
 function queryAddFrameToCompanySuccess(tx, results) {
-	frameToAddMarkUp = '';
-	
     var len = results.rows.length;
         
     for (var i=0; i<len; i++){
       	var row = results.rows.item(i);
             
+        addFrameRow(row);
+    	
+    	break;
+   	} // for
+}
+
+function addFrameRow(row) {
+	frameToAddMarkUp = '';
     	var nbDefensive = row.nb_defensive;
         var defensiveDice = '';
         if (nbDefensive > 0) {
@@ -300,16 +373,13 @@ function queryAddFrameToCompanySuccess(tx, results) {
 	    nbFrames++;
 	    companyForm.elements["nbFrames"].value = nbFrames;
             
-        frameToAddMarkUp = '<tr id="frame_' + nbFrames + '"><input name="frameId_' + nbFrames + '" type="hidden" value="' + row.id + '"/><td><a class="frameNameLink" href="./frameDetail.html?frameId=' + row.id + '">' + 
-         	tooManySystems + ' ' + row.name + ' ' + tooManySystems + '</a></td><td>' + tooManySystems + ' ' + nbSystems + '/4 ' + 
+        frameToAddMarkUp = '<tr id="frame_' + nbFrames + '"><input name="frameId_' + nbFrames + '" type="hidden" value="' + row.frameId + '"/><td><a class="frameNameLink" href="./frameDetail.html?frameId=' + row.frameId + '">' + 
+         	tooManySystems + ' ' + row.frameName + ' ' + tooManySystems + '</a></td><td>' + tooManySystems + ' ' + nbSystems + '/4 ' + 
            	tooManySystems + 
            	'</td><td><div class="systemDiceList">' + dice + '</div></td>' +
            	'<td><input class="form-control" name="nbRockets_' + nbFrames + '" type="text" value="0"/></td>' +
            	'<td><a href="Javascript:removeFrame(' + nbFrames + ');"><img alt="Delete" src="./img/icons/cross.png"/></a></td></tr>';
-    	
-    	break;
-   	} // for
-	
+
 	$('#frameTable').append(frameToAddMarkUp);
 }
 
