c4b52efd1320f195ebbae78a1f9d15a8fd079e8e
cedricms <cedric@maria-sube.com>
2013-10-27 21:45:54 +0100

    company_list_photo : render company photos in list.

--- assets/www/js/mof0ListCompanies.js
+++ assets/www/js/mof0ListCompanies.js
@@ -10,15 +10,15 @@ function listCompanies() {
 	
     // Query the database
     function queryDB(tx) {
-    	query = 'SELECT c.id AS companyId, c.name AS companyName, count(cf.id_frame) AS nbFrames, ifnull(sum(f.nb_defensive + f.nb_movement + f.nb_surveillance_communication + f.nb_hand_to_hand + f.nb_direct_fire + f.nb_artillery_range), 0) AS nbSystems FROM company c LEFT OUTER JOIN company_frame cf ON c.id = cf.id_company LEFT OUTER JOIN frame f ON cf.id_frame = f.id GROUP BY cf.id_company ORDER BY upper(c.name) asc';
+    	query = 'SELECT c.id AS companyId, c.name AS companyName, c.company_picture_url AS companyPictureUrl, count(cf.id_frame) AS nbFrames, ifnull(sum(f.nb_defensive + f.nb_movement + f.nb_surveillance_communication + f.nb_hand_to_hand + f.nb_direct_fire + f.nb_artillery_range), 0) AS nbSystems FROM company c LEFT OUTER JOIN company_frame cf ON c.id = cf.id_company LEFT OUTER JOIN frame f ON cf.id_frame = f.id GROUP BY cf.id_company ORDER BY upper(c.name) asc';
         tx.executeSql(query, [], querySuccess, errorDB);
     }
 
     // Query the success callback
     function querySuccess(tx, results) {
         var len = results.rows.length;
-        //alert('len : ' + len);
         
+        var companyPhotoIdMap = new Object();
         for (var i=0; i<len; i++){
         	var row = results.rows.item(i);
 
@@ -30,6 +30,53 @@ function listCompanies() {
     	        '</a></div>' +
     	        '<div><a class="companyNameLink" href="./companyDetail.html?companyId=' + companyId + '">' + 
             	row.companyName + '</a></div></td><td class="tableData">' + row.nbFrames + '</td><td class="tableData">' + row.nbSystems + '</td><td><a href="Javascript:deleteCompany(' + companyId + ');"><img alt="Delete" src="./img/icons/cross.png"/></a></td></tr>' );
+            	
+            var companyPictureUrl = row.companyPictureUrl;
+            if ((typeof companyPictureUrl != 'undefined') && (companyPictureUrl != null) && (companyPictureUrl != '') && (companyPictureUrl !== '')) {
+            	companyPhotoIdMap[companyPictureUrl.substring('/MobileFrameZeroTools/company/'.length, companyPictureUrl.length)] = companyId;
+            	
+            	window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, 
+            		function (fileSystem) {
+						fileSystem.root.getDirectory('MobileFrameZeroTools', null, 
+							function (mfztDirectory) {
+								mfztDirectory.getDirectory('company', null, 
+									function onGetCompanyDirectoryLoadCompanyPhoto(companyDirectory) {
+										companyImageName = companyPictureUrl.substring('/MobileFrameZeroTools/company/'.length, companyPictureUrl.length);
+										companyDirectory.getFile(companyImageName, null, 
+											function (fileEntry) {
+    											fileEntry.file(function (file){
+													var reader = new FileReader();
+    												reader.onloadend = function(evt) {
+       	 													companyImageData = evt.target.result;
+        
+    														// Get image handle
+	        												var companyPicture = document.getElementById('companyPicture_' + companyPhotoIdMap[file.name]);
+    	    												// Show the captured photo
+        													// The inline CSS rules are used to resize the image
+        													companyPicture.src = "data:image/jpeg;base64," + companyImageData;
+   														};
+    													reader.readAsBinaryString(file);
+													}, 
+    												function (error) {
+  														alert('Unable to load the following file : companyPictureUrl (' + error.code + ')');
+													});
+											}, 
+											function (error) {
+  												alert('Unable to find the following file : companyImageName (' + error.code + ')');
+											});
+									}, 
+									function (error) {
+  										alert('Unable to find the following directory : company (' + error.code + ')');
+									});
+							}, 
+							function (error) {
+  								alert('Unable to find the following directory : MobileFrameZeroTools (' + error.code + ')');
+							});
+					}, 
+	            	function (error) {
+  						alert('Unable to load  : ' + companyPictureUrl + ' (' + error.code + ')');
+					});
+			} // if
         } // for
     }
 
